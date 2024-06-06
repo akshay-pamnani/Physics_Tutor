7576588d951f60405e0812948ff76efe06f3e964
Akshay Pamnani <akshay.iithyd@gmail.com>
2024-06-06 17:56:57 +0530

    Improved Prompt

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # app/main.py
 import streamlit as st
 from langchain.llms import OpenAI
+from langchain.prompts import PromptTemplate
 
 st.title('🦜🔗Physics_Tutor')
 
@@ -8,7 +9,18 @@ openai_api_key = st.sidebar.text_input('OpenAI API Key', type='password')
 
 def generate_response(input_text):
     llm = OpenAI(temperature=0.7, openai_api_key=openai_api_key)
-    st.info(llm(input_text))
+
+    prompt_template = PromptTemplate(
+        input_variables = ['input'],
+        template='''You are a 9th Grade AI Physics Tutor and your name is ATP, 
+        I will be writing something about Physics and you have to tell me if I am right or wrong in two sentences.
+        Here is my write-up. {input}'''     
+    )
+
+    prompt = prompt_template.format(input = input_text)
+
+    st.info(llm.invoke(prompt))
+    #return 
 
 with st.form('my_form'):
     text = st.text_area('What doubts do you have?')
